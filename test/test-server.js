16e31c421ba5eb8df0bedd0670431bb272db91b7
AithaSrikanth <srikanthaith@gmail.com>
2022-03-04 10:30:02 +0000

    test changes

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to Cloud Connection_4 application</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>Welcome to Cloud Connection Application</h1>"));
         return done();
       });
 
